744093334901cecfdef5ae7aa7a606bd9eae4c1c
Dale Dybvig <dalerdybvig@gmail.com>
2019-10-23 12:40:52 -0500

    added database creation scripts for guessTheNumber and guessTheNumberTest

---
+++ GuessTheNumberRESTService/GuessTheNumberTESTDBCreationScript.sql
@@ -0,0 +1,22 @@
+-- drop database if exists guessTheNumberTest;
+-- create database guessTheNumberTest;
+
+use guessTheNumberTest;
+
+CREATE TABLE `game` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `answer` char(4) NOT NULL,
+  `inProgress` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `guess` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `guess` char(4) NOT NULL,
+  `timeOfGuess` datetime NOT NULL,
+  `result` varchar(10) DEFAULT NULL,
+  `gameID` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `gameID` (`gameID`),
+  CONSTRAINT `guess_ibfk_1` FOREIGN KEY (`gameID`) REFERENCES `game` (`id`)
+)
